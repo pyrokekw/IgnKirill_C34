1dd3b7ee5094c1aecb251638d0c8c6d75b7c726b
Kirill Ignatkevich <maratandkirill24@gmail.com>
2025-02-14 21:13:26 +0300

    hometask5 done

---
+++ src/main/java/HT5.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class HT5 {
+
+    public static void main(String[] args) {
+        //1) Дан одноместный массив целых чисел. Необходимо получить второй,
+        // который будут зеркальной копией первого (элементы наоборот - с последнего до первого)
+        int[] firstArray = {1,6,8,2,9};
+        int[] secondArray = new int[5];
+        for (int i=0, j = secondArray.length - 1; firstArray.length>i; i++, j--) {
+            secondArray[j] = firstArray[i];
+        }
+        System.out.println(Arrays.toString(secondArray));
+        //2) Дан массив целых чисел. Необходимо удалить элемент, расположенный по середине.
+        int[] array = {1,7,2,6,5};
+        int[] newArray = new int[array.length-1];
+        int mid = array.length / 2;
+        for (int i = 0, j = 0; firstArray.length>i; i++) {
+            if (i == mid) {
+                continue;
+            }
+            newArray[j]=array[i];
+            j++;
+        }
+        System.out.println(Arrays.toString(newArray));
+    }
+}
