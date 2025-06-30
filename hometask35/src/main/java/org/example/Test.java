052b8a06696b4238baa435de86fa762ebb94db9a
Kirill Ignatkevich <maratandkirill24@gmail.com>
2025-06-30 09:25:17 +0300

    hometask35 done

---
+++ hometask35/src/main/java/org/example/Test.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Test {
+
+    public Test() {
+        System.out.println("Constructor completed.");
+    }
+
+    public void init() {
+        System.out.println("Init method completed.");
+    }
+}
